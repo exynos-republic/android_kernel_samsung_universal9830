3843bff8bf30b70a1a1ba445dbd874b704c83d7c
oberdfr <alebennella@gmail.com>
2024-04-29 20:42:27 +0200

    build_x1slte.sh: export variables

--- build_x1slte.sh
+++ build_x1slte.sh
@@ -1,2 +1,10 @@
-make O=out exynos9830-x1slte_defconfig
+make exynos9830-x1slte_defconfig \
+    O=out \
+ 	ARCH=arm64 \
+	CC=clang \
+ 	LD=ld.lld \
+ 	LLVM=1 \
+ 	LLVM_IAS=1 \
+ 	CLANG_TRIPLE=aarch64-linux-gnu- \
+	CROSS_COMPILE=aarch64-linux-gnu- \ 
 ./build.sh
