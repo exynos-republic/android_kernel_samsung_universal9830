40732730f5d7f66d7f5ca0a635119d99e5a00de2
oberdfr <alebennella@gmail.com>
2024-05-14 16:05:03 +0200

    treewide: make kernel locally buildable

---
+++ build.sh
@@ -0,0 +1,10 @@
+make -j16 \
+	O=out \
+ 	ARCH=arm64 \
+	CC=clang \
+ 	LD=ld.lld \
+ 	LLVM=1 \
+ 	LLVM_IAS=1 \
+ 	CLANG_TRIPLE=aarch64-linux-gnu- \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=arm-linux-gnueabi- $@
