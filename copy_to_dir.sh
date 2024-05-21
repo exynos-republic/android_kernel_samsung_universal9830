4883d2853884f7226ab63b21ea20fdef13699f02
oberdfr <alebennella@gmail.com>
2024-05-21 20:13:36 +0200

    [DO NOT MERGE] scritps: add script to copy to android dir

---
+++ copy_to_dir.sh
@@ -0,0 +1,5 @@
+# bin/bash
+
+cp out/install/dtb.img ../android/hentai/device/samsung/x1s-kernel/dtb.img
+cp out/install/Image ../android/hentai/device/samsung/x1s-kernel/Image
+cp out/install/dtbo.img ../android/hentai/device/samsung/x1s-kernel/dtbo.img
