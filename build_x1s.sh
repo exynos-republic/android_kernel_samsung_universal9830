dcfa3375ccda728e9591779ed50755363f9f0545
oberdfr <alebennella@gmail.com>
2024-05-21 20:24:49 +0200

    dts: Add dtb/dtbo build script
    * while at it copy every file needed to build android in out/install folder.

--- build_x1s.sh
+++ build_x1s.sh
@@ -8,4 +8,11 @@ make exynos9830-x1sxxx_defconfig \
  	CLANG_TRIPLE=aarch64-linux-gnu- \
 	CROSS_COMPILE=aarch64-linux-gnu- \ 
 
+# Build Kernel 
 ./build.sh
+
+# Build Dtb/Dtbo images
+./builddt.sh x1s
+
+# Copy Kernel image into install folder
+cp out/arch/arm64/boot/Image out/install/Image
